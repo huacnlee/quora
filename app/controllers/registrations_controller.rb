e5c3377b40c50279f67733273a4449b181e58e1a
Jason Lee <huacnlee@gmail.com>
2011-03-29 14:18:33 +0800

    Encoding bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class RegistrationsController < ApplicationController
   prepend_before_filter :require_no_authentication, :only => [ :new, :create, :cancel ]
   prepend_before_filter :authenticate_scope!, :only => [:edit, :update, :destroy]
