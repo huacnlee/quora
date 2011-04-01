8f3a205439994c93748b413c79f5022550adddb9
Nowa <nowazhu@gmail.com>
2011-04-01 14:17:38 +0800

    Mod: 话题里每页显示10条问题

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
 
   def show
     name = params[:id].strip
-    @per_page = 3
+    @per_page = 10
     @topic = Topic.find_by_name(name)
     @asks = Ask.all_in(:topics => [name]).desc(:id).paginate(:page => params[:page], :per_page => @per_page)
 
