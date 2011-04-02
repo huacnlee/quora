1802040ad16c1643225dc2aaf5044e89b3d344d7
Jason Lee <huacnlee@gmail.com>
2011-04-02 13:45:13 +0800

    Mod: 搜索改进

--- app/controllers/asks_controller.rb
+++ app/controllers/asks_controller.rb
@@ -12,10 +12,15 @@ class AsksController < ApplicationController
 
   def search
     if params[:format] == "json"
-      @asks = Ask.search_title(params["w"],:limit => 10)
-      render :json => @asks.to_json(:only => [:topics,:_id,:title])
+      result = Ask.search_title(params["w"],:limit => 10)
+      simple_items = result[:items].collect do |item|
+        {:topics => item.topics,
+          :title => item.title,
+          :_id => item.id}
+      end
+      render :json => simple_items.to_json
     else
-      @asks = Ask.search_title(params["w"],:limit => 20)
+      @asks = Ask.search_title(params["w"],:limit => 20)[:items]
       set_seo_meta("关于“#{params[:w]}”的搜索结果")
       render "index"
     end
