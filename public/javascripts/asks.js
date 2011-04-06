8d790e923d8bb49a2fda738b75ec4444dcaeacb4
Jason Lee <huacnlee@gmail.com>
2011-04-06 16:56:18 +0800

    Mod: 搜索可以显示用户头像和tagline了

--- public/javascripts/asks.js
+++ public/javascripts/asks.js
@@ -116,11 +116,17 @@ function searchAjaxCallback(el,res){
         html += '<a href="/topics/'+res[i].title+'">'+item_title+'</a><span class="type">话题</span>';
       }
       else if(item_type == "User"){
+        /* 用户 */
         avatar = res[i].avatar_small;
         if(/http:\/\//.test(avatar) == false){
           avatar = "/images/" + avatar;
         }
-        html += '<img src="'+ avatar +'" /><a href="/users/'+res[i].slug+'">'+item_title+'</a><span class="type">会员</span>';
+        tagline = "";
+        if(res[i].tagline != null){
+          tagline = res[i].tagline;
+        }
+        html += '<img class="avatar" src="'+ avatar +'" />';
+        html += '<div class="uinfo"><p><a href="/users/'+res[i].slug+'">'+item_title+'</a></p><p class="tagline">'+tagline+'</p></div>';
       }
       else{
         if(res[i].topics != null){
