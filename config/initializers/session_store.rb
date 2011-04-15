3ed406784f522923e176edc5dff242844aa609fe
Jason Lee <huacnlee@gmail.com>
2011-04-15 12:32:57 +0800

    Fix: Session不再过期

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Quora::Application.config.session_store :cookie_store, :key => '_quora_session'
-
+Quora::Application.config.session_store :cookie_store, 
+                                        :key => '_quora_session',
+                                        :expire_after => 86400*90
+    
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
