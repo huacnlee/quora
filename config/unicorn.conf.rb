9550515ddb0696e9c9a473196931d6bf58fbd1b8
Jason Lee <huacnlee@gmail.com>
2011-08-09 10:00:51 +0800

    -

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -23,6 +23,7 @@ before_fork do |server, worker|
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
+      system("echo 'Quora Unicorn reload failed.'")
       # someone else did our job for us
     end
   end
