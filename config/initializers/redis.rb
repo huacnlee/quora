85188fcc58ef924661228f13c2d21c5b2a32fdc5
Jason Lee <huacnlee@gmail.com>
2011-10-10 14:07:13 +0800

    缩进修正

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -9,7 +9,7 @@ redis_search = Redis.new(:host => redis_config['host'],:port => redis_config['po
 redis_search.select("3")
 Redis::Search.configure do |config|
   config.redis = redis_search
-	config.complete_max_length = 30
+  config.complete_max_length = 30
 end
 
 # Resque
