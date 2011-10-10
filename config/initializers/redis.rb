8a626e3790dcf9209f3e3ca6441daafeb7e19d05
Jason Lee <huacnlee@gmail.com>
2011-10-10 14:06:53 +0800

    修正 YAML 配置读取的错误写法

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,10 +2,10 @@ require "redis"
 require "redis-search"
 
 redis_config = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
-$redis = Redis.new(:host => redis_config[:host],:port => redis_config[:port])
+$redis = Redis.new(:host => redis_config['host'],:port => redis_config['port'])
 $redis.select("0")
 
-redis_search = Redis.new(:host => redis_config[:host],:port => redis_config[:port])
+redis_search = Redis.new(:host => redis_config['host'],:port => redis_config['port'])
 redis_search.select("3")
 Redis::Search.configure do |config|
   config.redis = redis_search
@@ -13,5 +13,5 @@ Redis::Search.configure do |config|
 end
 
 # Resque
-Resque.redis = Redis.new(:host => redis_config[:host],:port => redis_config[:port])
+Resque.redis = Redis.new(:host => redis_config['host'],:port => redis_config['port'])
 Resque.redis.namespace = "resque:quora"
